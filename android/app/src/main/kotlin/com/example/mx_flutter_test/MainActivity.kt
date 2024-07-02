cd451e5e088a1873ad8a48090003a706240223b7
zakwan <zakwan@aerosys.com.my>
2024-07-02 15:30:54 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/example/mx_flutter_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mx_flutter_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/model/product_model.dart
@@ -0,0 +1,67 @@
+class Product {
+  final int? id;
+  final String? title;
+  final double? price;
+  final String? description;
+  final String? category;
+  final String? image;
+  final Rating? rating;
+
+  Product({
+    this.id,
+    this.title,
+    this.price,
+    this.description,
+    this.category,
+    this.image,
+    this.rating,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'] as int?,
+      title: json['title'] as String?,
+      price: (json['price'] as num?)?.toDouble(),
+      description: json['description'] as String?,
+      category: json['category'] as String?,
+      image: json['image'] as String?,
+      rating: json['rating'] != null ? Rating.fromJson(json['rating']) : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'price': price,
+      'description': description,
+      'category': category,
+      'image': image,
+      'rating': rating?.toJson(),
+    };
+  }
+}
+
+class Rating {
+  final double? rate;
+  final int? count;
+
+  Rating({
+    this.rate,
+    this.count,
+  });
+
+  factory Rating.fromJson(Map<String, dynamic> json) {
+    return Rating(
+      rate: (json['rate'] as num?)?.toDouble(),
+      count: json['count'] as int?,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'rate': rate,
+      'count': count,
+    };
+  }
+}
